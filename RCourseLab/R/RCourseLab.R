f213d03b1d122d40e13e9fa368d018e9f1171f73
tondralok <farch587@student.liu.se>
2017-09-15 13:46:13 +0200

    Lab 3 Assignment using github.

---
+++ RCourseLab/R/RCourseLab.R
@@ -0,0 +1,12 @@
+#' RCourseLab: A package for Lab 3 Assigments.
+#'
+#' The RCourseLab package provides two functions:
+#' euclidean & dijkstra.
+#' 
+#' @section euclidean functions:
+#' Euclidean() returns the greatest common divisor (GCD) of two numbers.
+#' @section dijkstra functions:
+#' dijkstra() returns the shortest path based on the algorithm.
+#' @docType package
+#' @name RCourseLab
+NULL
---
+++ RCourseLab/R/euclidean.R
@@ -0,0 +1,60 @@
+#' Euclidean algorithm for GCD.
+#' 
+#' Euclidean() returns the greatest common divisor (GCD) of two numbers.
+#' @param x numeric scalars of integers
+#' @param y numeric scalars of integers
+#'
+#' @return If input data is numeric or integer then 
+#' the output will be a integer otheriwise it will return an error.
+#' @export
+#'
+#' @examples euclidean(123612, 13892347912)
+#' euclidean(100, 1000)
+
+euclidean<- function(x,y){
+
+  data <- c(x,y)
+               if(x>y){
+                 a<-x
+                 b<-y}
+               else {
+                 a<-y
+                 b<-x}
+
+if(is.numeric(data))
+  {if((floor(data[1])==data[1]) && (floor(data[2])==data[2]))
+     {
+    flag=FALSE
+    while(flag==FALSE){
+      if(a==0 || b==0)
+        {
+          if(a==0)
+          {
+            GCD<-b
+            flag<-TRUE
+          }
+          else (b==0)
+          {
+            GCD<-a
+            flag<-TRUE
+          }
+        }
+      
+     else
+       {
+           c<-a%%b
+           d<-a/b
+           a<-b
+           b<-c
+        }
+  }
+    return(GCD)
+  }
+  else
+    stop("Data is not integer ! \n", call. = FALSE)
+    
+  }
+ else 
+   stop("Data is not numeric ! \n",call. = FALSE)
+}
+
